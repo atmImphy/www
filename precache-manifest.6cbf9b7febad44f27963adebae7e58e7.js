96723e3af2a7394e814ee6af4c2cb2be184e722a
mark <1041416864@qq.com>
2019-09-12 10:15:19 +0800

    12

---
+++ precache-manifest.6cbf9b7febad44f27963adebae7e58e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "02497c27c70155ffd6d0",
+    "url": "/js/chunk-vendors.e6f7c778.js"
+  },
+  {
+    "revision": "3c8d24ee7a16d5150f59",
+    "url": "/js/app.74338c75.js"
+  },
+  {
+    "revision": "d9a70f8c3099c3768dea",
+    "url": "/js/about.73130f49.js"
+  },
+  {
+    "revision": "3fe5bc02318db6843610a706f03128ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3c8d24ee7a16d5150f59",
+    "url": "/css/app.fd51bb97.css"
+  }
+];
